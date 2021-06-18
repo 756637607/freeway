2d0668e6658bad55170eb0d95f0b503944493753
756637607 <756637607@qq.com>
2021-06-18 17:34:34 +0800

    test123

--- src/main/java/com/system/freeway/controller/AnalysisController.java
+++ src/main/java/com/system/freeway/controller/AnalysisController.java
@@ -57,5 +57,5 @@ public class AnalysisController {
     public ResponseVO getStructureInfo(StructureDTO param){
         return studentService.getStructureInfo(param);
     }
-    //test1
+    //test123
 }
